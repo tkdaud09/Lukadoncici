e4fb8973268108f0c00c9582106cf3bc684ac0c2
itwill <itwill@DESKTOP-C9GOHGO>
2023-03-31 11:36:08 +0900

    exam

--- java_1/src/example/OperatorExample.java
+++ java_1/src/example/OperatorExample.java
@@ -4,11 +4,20 @@ public class OperatorExample {
    public static void main(String[] args) {
       //245678초를 일시분초 형식으로 변환하여 출력하세요.
       int sec=245678;
-      int day=sec/(24*60*60);
+      int day=sec/(24*60*60); //86400
       int hour=sec/(60*60);
       int minute=sec % (60*60)/60;
       int second=sec % 60;
       System.out.println(day+"일 "+hour+"시 "+minute+"분 "+second+"초");
+      
+      int day1=sec/(24*60*60);
+      sec%=86400;
+      int hour1=sec/(60*60);//3600
+      sec%=3600;
+      int min1=sec/60;
+      int sec1=sec%60;
+      
+     System.out.println(day1+"일 "+hour1+"시 "+min1+"분 "+sec1+"초");
 
       System.out.println("===============================================");
       //한대의 가격이 1억 5천원만인 비행기를 20대 구매할 경우 지불해야될 금액을 계산하여 출력하세요. 
@@ -18,7 +27,14 @@ public class OperatorExample {
       
       System.out.println("총금액 = "+(cnt>=15?(exp*cnt)*(75.0/100.0):(exp*cnt))+"억");      
       //System.out.println(cnt>=15?sum(exp*cnt)*(25/100):sum(exp*cnt));
-   
+      
+      int cnt1=20;
+      int exp1=150000000;
+    		  
+      long money=(long)(cnt>=14?exp1*0.75:exp1)*cnt1;
+      System.out.println("지불 금액 = "+money);
+    
+      
       System.out.println("===============================================");
    }
 }
--- java_1/src/example/VariableExample.java
+++ java_1/src/example/VariableExample.java
@@ -10,14 +10,14 @@ public class VariableExample {
 		System.out.println("======================================================");
 		//높이가 9이고 밑변의 길이가 7인 삼각형의 넓이를 계산하여 출력하세요.
 		int nob=9, mit=7;
-		double nulbe2=((double)nob*mit)/2;
+		double nulbe2=(nob*mit)/2.0;
 		System.out.println("삼각형의 넓이 = "+nulbe2);
 
 		System.out.println("======================================================");
 		//10명의 전체 몸무게가 759Kg인 경우 평균 몸무게를 계산하여 출력하세요.
 		int per=10, tot=759;
-		double eve=(double)tot/per;
-		System.out.println("평균 몸무게 = "+eve);
+		double ave=(double)tot/per;
+		System.out.println("평균 몸무게 = "+ave+"Kg");
 
 		System.out.println("======================================================");
 		//이름이 [홍길동]인 학생이 국어점수 89점, 영어점수 93점, 수학점수 95점을 받은 경우
@@ -26,10 +26,10 @@ public class VariableExample {
 		String name="홍길동";
 		int kok=89, eng=93, mat=95;
 		int tot2=kok+eng+mat;
-		double eve2=(double)tot2/3;
-		System.out.println("이름 = "+name);
-		System.out.println("총점 = "+tot2);
-		System.out.println("평균 = "+(int)(eve2*10)/10.0);
+		double eve=tot2/3.0;
+		System.out.println("이름 = "+name+", 총점 = "+tot2+", 평균 = "+(int)(eve*10)/10.0);
+//		System.out.println("총점 = "+tot2);
+//		System.out.println("평균 = "+(int)(eve*10)/10.0);
 
 		System.out.println("======================================================");
 	}
