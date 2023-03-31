cf43ba40199c0cc69493a44ba5e7e4ab756327fd
itwill <itwill@DESKTOP-C9GOHGO>
2023-03-31 10:28:50 +0900

    3day

---
+++ java_1/src/basic/HelloWorld.java
@@ -0,0 +1,15 @@
+package basic;
+
+//숙제
+// => https://www.oracle.com 사이트 회원가입
+// => https://github.com/ 사이트 회원가입
+
+//깃허브(GitHub) : 분산 버전 관리 툴인 깃 저장소 호스팅을 제공하는 웹 서비스
+//깃(Git) : 여러명의 사용자들 간에 파일의 작업을 조율하기 위한 분산 버전 관리 시스템(프로그램)
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Java");
+		System.out.println("_Progamming");
+		System.out.println("Good Luck!!!");
+	}
+}
