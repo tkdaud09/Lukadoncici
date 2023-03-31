702bdca2afce44dbfee5e2353bb46de821699cc1
itwill <itwill@DESKTOP-C9GOHGO>
2023-03-31 14:18:09 +0900

    3day

--- java_1/src/basic/OperatorApp6.java
+++ java_1/src/basic/OperatorApp6.java
@@ -8,7 +8,7 @@ package basic;
 //일반적으로 연산식은 왼쪽에서 오른쪽 방향으로 처리되지만 단항 연산자와 대입연산자를
 //사용한 연산식은 오른쪽에서 왼쪽 방향으로 처리
 //ex) A+B+C              A=B=C
-//   -----→            ←-----
+//   -----→            ←----- 
 
 public class OperatorApp {
 	public static void main(String[] args) {
