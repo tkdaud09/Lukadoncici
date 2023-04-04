007f561d36afbfec5a7e368d814d0c98a03dadcc
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-04 11:30:38 +0900

    exam6day

--- java_1/src/example/MultiforExample.java
+++ java_1/src/example/MultiforExample.java
@@ -1,62 +1,71 @@
 package example;
 
 public class MultiforExample {
-	public static void main(String[] args) {
-		//★★★★★
-		//★★★★
-		//★★★
-		//★★
-		//★
-		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=(6-i);j++) {
-				System.out.print("★");
-			}
-			System.out.println();
-		}
+   public static void main(String[] args) {
+      //★★★★★
+      //★★★★
+      //★★★
+      //★★
+      //★
+      for(int i=1;i<=5;i++) {
+         for(int j=1;j<=(6-i);j++) {
+            System.out.print("★");
+         }
+         System.out.println();
+      }
 
-		System.out.println("===============================================================");
-		//★★★★★
-		//☆★★★★
-		//☆☆★★★
-		//☆☆☆★★
-		//☆☆☆☆★
-		for(int i=1;i<=5;i++) {
-			for(int j=4;j>=(6-i);j--) {
-				System.out.print("☆");
-			}
-			for(int j=1;j<=(6-i);j++) {
-				System.out.print("★");
-			}
-			System.out.println();
-		}
+      System.out.println("===============================================================");
+      //★★★★★
+      //☆★★★★
+      //☆☆★★★
+      //☆☆☆★★
+      //☆☆☆☆★
+      for(int i=1;i<=5;i++) {
+         for(int j=4;j>=(6-i);j--) {
+            System.out.print("☆");
+         }
+         for(int j=1;j<=(6-i);j++) {
+            System.out.print("★");
+         
+         }
+         System.out.println();
+      }
 
-		System.out.println("===============================================================");
-		//★
-		//★★★
-		//★★★★★
-		//★★★★★★★
-		//★★★★★★★★★
-		
-		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=(i*2)-1;j++) {
-				System.out.print("★");
-				
-			}
-		
-			System.out.println();
-				
-		}
+      System.out.println("===============================================================");
+      //★
+      //★★★
+      //★★★★★
+      //★★★★★★★
+      //★★★★★★★★★
+      
+      for(int i=1;i<=5;i++) {
+         for(int j=1;j<=(i*2)-1;j++) {
+            System.out.print("★");
+            
+         }
+      
+         System.out.println();
+            
+      }
 
-		System.out.println("===============================================================");
-		//★☆☆☆★
-		//☆★☆★☆
-		//☆☆★☆☆
-		//☆★☆★☆
-		//★☆☆☆★
- 
-		
-		System.out.println("===============================================================");
-	
-		System.out.println(1%5);
-	}
+      System.out.println("===============================================================");
+      //★☆☆☆★
+      //☆★☆★☆
+      //☆☆★☆☆
+      //☆★☆★☆
+      //★☆☆☆★
+      for(int i=1;i<=5;i++) {
+         for(int j=1;j<=5;j++) {
+            if(i==j || (6-i)==j) {
+               System.out.print("★");
+         } else { 
+            System.out.print("☆");
+         }
+         }
+         System.out.println();
+      }
+      
+      System.out.println("===============================================================");
+   
+   }
 }
--- java_1/src/example/TotalOneExample.java
+++ java_1/src/example/TotalOneExample.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.Scanner;
+
 //키보드로 정수값을 5번 입력받아 합계를 계산하여 출력하는 프로그램을 작성하세요.
 //ex) 1번째 정수값 입력 >> 10
 //    2번째 정수값 입력 >> 20
@@ -9,7 +11,16 @@ package example;
 //    [결과]합계 = 150
 public class TotalOneExample {
 	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
 		
-
+		int sum=0;
+		
+		for(int i=1;i<=5;i++) {
+			System.out.print(i+"번째 정수값 입력 >> ");
+			int num=scanner.nextInt();
+			sum+=num;//입력값을 sum 변수에 누적하여 저장
+		}
+		
+		System.out.println("[결과] ="+sum);
 	}
 }
