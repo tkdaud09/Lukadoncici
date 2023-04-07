02ae1cfb632e3703606df87e4f54cb68c1d13cfb
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-07 10:32:12 +0900

    8day

--- Java_2/src/oop/Overload.java
+++ Java_2/src/oop/Overload.java
@@ -15,8 +15,8 @@ public class Overload {
 	
 	//메소드 오버로딩(Method Overloading) : 클래스에서 동일한 기능을 제공하는 메소드가
 	//매개변수에 의해 여러개 선언해야할 경우 메소드의 이름을 같도록 선언하는 기능
-	// => 매개변수의 자료형 또는 갯수가 같지 않도록 선언
-	// => 접근제한자와 반환형은 오버로드 선언과 무관
+	// => ☆매개변수의 자료형 또는 갯수가 같지 않도록 선언☆
+	// => 접근제한자(public)와 반환형(void)은 오버로드 선언과 무관
 	public void display(int param) {
 		System.out.println("정수값 = "+param);
 	}
