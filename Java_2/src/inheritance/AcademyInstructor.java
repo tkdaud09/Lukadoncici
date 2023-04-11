8606fc99b2d60a2a695dbcf631ad6379087a9a7e
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-11 11:41:08 +0900

    11day

--- Java_2/src/inheritance/AcademyInstructor.java
+++ Java_2/src/inheritance/AcademyInstructor.java
@@ -1,6 +1,30 @@
 package inheritance;
 
 //강사정보(강사번호, 강사이름, 강의과목)를 저장하기 위한 클래스
-public class AcademyInstructor {
+public class AcademyInstructor extends AcademyPerson {
+	private String subject;
+	
+	public AcademyInstructor() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public AcademyInstructor(int num, String name, String subject) {
+		super(num, name);
+		this.subject = subject;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("강사번호 = "+getNum());
+		System.out.println("강사이름 = "+getName());
+		System.out.println("강의과목 = "+subject);
+	}
 }
--- Java_2/src/inheritance/AcademyStaff.java
+++ Java_2/src/inheritance/AcademyStaff.java
@@ -1,6 +1,30 @@
 package inheritance;
 
 //직원정보(직원번호, 직원이름, 근무부서)를 저장하기 위한 클래스
-public class AcademyStaff {
+public class AcademyStaff extends AcademyPerson{
+	private String depart;
+	
+	public AcademyStaff() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public AcademyStaff(int num, String name, String depart) {
+		super(num, name);
+		this.depart = depart;
+	}
+
+	public String getDepart() {
+		return depart;
+	}
+
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("직원번호 = "+getNum());
+		System.out.println("직원이름 = "+getName());
+		System.out.println("근무부서 = "+depart);
+	}
 }
