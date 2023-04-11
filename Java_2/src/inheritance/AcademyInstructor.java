0970b54fd54dc46f88fdea61a0e4fe0750bf74df
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-11 11:41:45 +0900

    11day

--- Java_2/src/inheritance/AcademyInstructor.java
+++ Java_2/src/inheritance/AcademyInstructor.java
@@ -1,6 +1,7 @@
 package inheritance;
 
 //강사정보(강사번호, 강사이름, 강의과목)를 저장하기 위한 클래스
+//=> 강사번호와 강사이름 관련 속성과 행위는 AcademyPerson 클래스를 상속받아 사용
 public class AcademyInstructor extends AcademyPerson {
 	private String subject;
 	
