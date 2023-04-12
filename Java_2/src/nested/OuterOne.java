3bfb1b57eb9b4c50b64c89eb8e28982c608068d5
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-12 18:09:08 +0900

    12day

--- Java_2/src/nested/OuterOne.java
+++ Java_2/src/nested/OuterOne.java
@@ -26,15 +26,15 @@ public class OuterOne {
 	public void outerDisplay() {
 		System.out.println("outerNum = "+outerNum);
 		
-		//외부클래스에서는 객체 내부 클래스의 필드 또는 메소드에 대한 ★직접적인 참조불가능
+		//외부클래스에서는 객체 내부 클래스의 필드 또는 메소드에 대한 직접적인 참조불가능
 		//System.out.println("innerNum = "+innerNum);
 		//innerDisplay();
 		
-		//외부클래스는 객체 내부클래스로 ★객체를 생성하여 접근제한자에 상관없이 필드 또는
-		//메소드 참조 가능
-		//InnerOne innerOne=new InnerOne();
-		//System.out.println("innerNum = "+innerOne.innerNum);
-		//innerOne.innerDisplay();
+		//외부클래스는 객체 내부클래스로 객체를 생성하여 접근제한자에 상관없이 필드 또는
+		//메소드 참조가능
+		InnerOne innerOne=new InnerOne();
+		System.out.println("innerNum = "+innerOne.innerNum);
+		innerOne.innerDisplay();
 	}
 	
 	//인스턴스 내부 클래스 => 컴파일 결과를 [외부클래스.내부클래스.class] 파일로 제공
