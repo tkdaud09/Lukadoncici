b6055c4ad6a4ba315a914d3baf3645ffcd60bf39
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-12 17:53:10 +0900

    12day

---
+++ Java_2/src/enumerate/CompassApp.java
@@ -0,0 +1,24 @@
+package enumerate;
+
+public class CompassApp {
+	public static void main(String[] args) {
+		//열거형에 선언된 상수필드값 출력 = 상수필드명을 제공받아 출력
+		System.out.println("동쪽 = "+Compass.EAST);
+		System.out.println("서쪽 = "+Compass.WEST);
+		System.out.println("남쪽 = "+Compass.SOUTH);
+		System.out.println("북쪽 = "+Compass.NORTH);
+		System.out.println("=====================================");
+		System.out.println("동쪽 = "+Compass.EAST.getvalue());
+		System.out.println("서쪽 = "+Compass.WEST.getvalue());
+		System.out.println("남쪽 = "+Compass.SOUTH.getvalue());
+		System.out.println("북쪽 = "+Compass.NORTH.getvalue());
+		System.out.println("=====================================");
+		
+		//EnumType.values() : 열거형에 선언된 모든 상수필드를 배열로 변환하여 반환하는 메소드
+		for(Compass compass : Compass.values()) {
+			//EnumType.ordinal() : 상수필드를 구분하기 위한 첨자(Index)를 반환하는 메소드
+			System.out.println(compass+" = "+compass.getvalue()+" >> "+compass.ordinal());
+		}
+		System.out.println("=====================================");
+	}
+}
