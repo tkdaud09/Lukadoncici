8db6e915bf2fb39ba6203e4c7a4dfb8c1cc78f8c
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-14 18:00:02 +0900

    14day

---
+++ java_3/src/xyz/itwill/lang/ConsoleCalculateApp.java
@@ -0,0 +1,13 @@
+package xyz.itwill.lang;
+
+//키보드로 사칙 연산식을 입력받아 연산결과를 계산하여 출력하는 프로그램 작성
+//ex) 연산식 입력 >> 20 + 10
+//    [결과]30
+// => 입력 연산식에서 사용 가능한 연산자는 사칙 연산자(*,/,+,-)만 허용
+// => 형식에 맞지 않는 연산식이 입력된 경우 에러 메세지 출력 후 프로그램 종료
+// => 입력 연산식에 공백 입력이 가능하도록 처리
+public class ConsoleCalculateApp {
+	public static void main(String[] args) {
+		
+	}
+}
