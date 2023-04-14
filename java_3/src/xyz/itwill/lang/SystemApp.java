9f9d60d8ce01394eef5163e3709d3c3b6a93b1c4
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-14 14:46:33 +0900

    14day

---
+++ java_3/src/xyz/itwill/lang/SystemApp.java
@@ -0,0 +1,19 @@
+package xyz.itwill.lang;
+
+//Java API(Application Programming Interface)
+//Java 프로그램 개발에 프로그램 기능 제공 하기 위한 도구
+// => 라이브러리를 이용하여 배포된 Java 자료형(클래스, 인터페이스, 열거형) 
+// => API문서를 활용항 Java 자료형에 대한 설명을 제공받아 사용
+
+
+//java.lang 패키지 : Java 프로그램 개발에 필요한 기본 자료형을 제공하는 패키지
+// => java.lang 패키지의 자료형은 import 처리를 하지 않아도 접근 가능
+
+//Object 클래스 : 모든 Java 클래스가 반드시 상속받는 부모클래스
+// = > 모든 Java 클래스를 대신하여 사용할수 있는 대표클래스 
+//Math 클래스 : 수학 관련 기능을 메소드로 제공하는 클래스
+
+
+public class SystemApp {
+
+}
