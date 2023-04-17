7e65c6580c46fca93398a45a95c145c515d4e82c
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-17 13:13:31 +0900

    15day

---
+++ java_3/src/xyz/itwill/lang/thread/SingleThreadApp.java
@@ -0,0 +1,9 @@
+package xyz.itwill.lang.thread;
+
+//스레드(Thread) : 프로세스에서 명령을 실행하기 위한 최소의 작업단위 - 프로그램의 흐름
+//프로세스(Process) : 메모리에 저장된 프로그램 - 중앙처리장치(CPU)에 의해 실행되기 위한 명령
+// =>
+
+public class SingleThreadApp {
+
+}
