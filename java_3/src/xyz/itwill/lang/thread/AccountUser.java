794cca58671c5da128a8a988c67e34c109306760
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-19 09:38:46 +0900

    15day

--- java_3/src/xyz/itwill/lang/thread/AccountUser.java
+++ java_3/src/xyz/itwill/lang/thread/AccountUser.java
@@ -1,7 +1,7 @@
 package xyz.itwill.lang.thread;
 
-//은행계좌 사용자정보(은행계좌정보, 사용자명)를 저장하기 위한 클래스 
-public class AccountUser {
+//은행계좌 사용자정보(은행계좌정보, 사용자명)를 저장하기 위한 클래스
+public class AccountUser extends Thread {
 	private Account account;//은행계좌정보 - 포함관계
 	private String userName;//사용자명
 	
@@ -31,5 +31,14 @@ public class AccountUser {
 		this.userName = userName;
 	}
 	
-	
+	@Override
+	public void run() {
+		//프로그램 개발자에 의해 생성된 스레드로 run() 메소드를 호출하여 명령 실행
+		// => 은행계좌 사용자가 사용하는 은행계좌에 입금 처리하는 메소드 호출
+		//account.deposit(userName, 5000);
+		
+		synchronized (account) {
+			account.withDraw(userName, 5000);
+		}
+	}
 }
