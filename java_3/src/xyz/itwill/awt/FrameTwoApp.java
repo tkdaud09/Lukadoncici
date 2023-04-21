ba259b9c1fe3d6377caf9aca385eb30c9c629054
itwill <itwill@DESKTOP-C9GOHGO>
2023-04-21 17:22:59 +0900

    18day

--- java_3/src/xyz/itwill/awt/FrameTwoApp.java
+++ java_3/src/xyz/itwill/awt/FrameTwoApp.java
@@ -8,7 +8,22 @@ public class FrameTwoApp extends Frame{
 
 	//생성자에서 프레임을 구성하는 디자인 설정 - UI 구현
 	public FrameTwoApp(String title) {
-		super("title");
+		super(title);//Frame(String title) 생성자를 이용하여 Frame 객체 생성
+		
+		//Frame.setTitle(String title) : 프레임의 제목을 변경하는 메소드
+		//setTitle(title);
+		
+		//Component.setBounds(int x, int y, int width, int height) : 컴퍼넌트의 출력 위치와
+		//크기를 변경하는 메소드
+		//setLocation(600.100);
+		//setSize(400, 300);
+		setBounds(600, 100, 400, 300);
+		
+		//Frame.setResizable(boolean resize) : 프레임의 크기 변경 여부를 설정하기 위한 메소드
+		// => false : 프레임 크기 변경 불가능, true : 프레임 크기 변경 가능 - 기
+		//setResizable(false);
+		setResizable(true);
+		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
