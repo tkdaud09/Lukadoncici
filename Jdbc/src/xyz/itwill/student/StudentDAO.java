42e3f2e18982d0920bf830801d7bebe6bd04aa96
user <user@BOOK-LCCUUB9T5H>
2023-05-16 17:02:23 +0900

    34day

--- Jdbc/src/xyz/itwill/student/StudentDAO.java
+++ Jdbc/src/xyz/itwill/student/StudentDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 // => 추상메소드를 이용하여 인터페이스를 상속받은 모든 자식클래스(DAO 클래스)가 동일한 
 //메소드가 선언되도록 메소드의 작성 규칙 제공
 // => 프로그램에서 사용하는 DAO 클래스가 변경돼도 프로그램에 영향을 최소화 하기 위해 인터페이스 작성
-public interface studentDAO {
+public interface StudentDAO {
 	//학생정보를 전달받아 STUDENT 테이블에 삽입하고 삽입행의 갯수를 반환하는 메소드
 	//int insertStudent(int no, String name, String phone, String address, String birthday);
 	int insertStudent(StudentDTO student);
