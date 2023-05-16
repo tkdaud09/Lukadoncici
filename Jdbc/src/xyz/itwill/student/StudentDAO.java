b58afbfcad24e533bdf1cd7c355e75a1a18b6ae2
user <user@BOOK-LCCUUB9T5H>
2023-05-16 18:09:03 +0900

    34day

--- Jdbc/src/xyz/itwill/student/StudentDAO.java
+++ Jdbc/src/xyz/itwill/student/StudentDAO.java
@@ -3,12 +3,11 @@ package xyz.itwill.student;
 import java.util.List;
 
 //DAO 클래스가 상속받기 위한 인터페이스
-// => 추상메소드를 이용하여 인터페이스를 상속받은 모든 자식클래스(DAO 클래스)가 동일한 
+// => 추상메소드를 이용하여 인터페이스를 상속받은 모든 자식클래스(DAO 클래스)가 동일한
 //메소드가 선언되도록 메소드의 작성 규칙 제공
-// => 프로그램에서 사용하는 DAO 클래스가 변경돼도 프로그램에 영향을 최소화 하기 위해 인터페이스 작성
+// => 프로그램에서 사용하는 DAO 클래스가 변경돼도 프로그램에 영향을 최소화 하기 위해 인터페이스 선언
 public interface StudentDAO {
 	//학생정보를 전달받아 STUDENT 테이블에 삽입하고 삽입행의 갯수를 반환하는 메소드
-	//int insertStudent(int no, String name, String phone, String address, String birthday);
 	int insertStudent(StudentDTO student);
 	
 	//학생정보를 전달받아 STUDENT 테이블에 저장된 학생정보를 변경하고 변경행의 갯수를 반환하는 메소드
@@ -22,8 +21,9 @@ public interface StudentDAO {
 	StudentDTO selectStudent(int no);
 	
 	//이름을 전달받아 STUDENT 테이블에 저장된 해당 이름의 학생정보를 검색하여 반환하는 메소드
+	// => 다중행은 List 객체 반환
 	List<StudentDTO> selectNameStudentList(String name);
-	
+		
 	//STUDENT 테이블에 저장된 모든 학생정보를 검색하여 반환하는 메소드
 	List<StudentDTO> selectAllStudentList();
 }
--- Jdbc/src/xyz/itwill/student/StudentDAOImp1.java
+++
@@ -1,9 +0,0 @@
-package xyz.itwill.student;
-
-//DAO(Data Access Object) 클래스 : 저장매체에 행정보를 삽입,삭제,변경,검색하는 기능을 제공하는
-// => 저장매체 : 정보를 행단위로 저장하여 관리하기 위한 하드웨어 또는 소프트 웨어 - DBMS
-// => 인터페이스를 상속받아 작성하는 것을 권장 - 메소드 작성 규칙 제공 : 유지보수의 효율성 증가
-public class StudentDAOImp1 implements StudentDAO {
-	
-	
-}
