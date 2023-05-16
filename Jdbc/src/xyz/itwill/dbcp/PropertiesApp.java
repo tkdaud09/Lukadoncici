8aa6104151f53cf3b92a47ffaafffda03331236a
user <user@BOOK-LCCUUB9T5H>
2023-05-16 12:39:20 +0900

    34day

--- Jdbc/src/xyz/itwill/dbcp/PropertiesApp.java
+++ Jdbc/src/xyz/itwill/dbcp/PropertiesApp.java
@@ -1,4 +1,4 @@
-package xyz.itwill.jdbc;
+package xyz.itwill.dbcp;
 
 //Properties 파일 : 프로그램 실행에 필요한 값을 제공하기 위한 텍스트 파일 - 확장자 : ~.Properties
 // => 프로그램을 변경하지 않고 Properties 파일의 내용을 변경하여 프로그램 실행 결과 변경 가능
@@ -7,7 +7,7 @@ package xyz.itwill.jdbc;
 // => Properties 파일에서는 영문자, 숫자, 일부 특수문자를 제외한 나머지 문자는 
 
 //user.properties 파일에 저장된 값을 얻어 출력하는 프로그램 작성
-public class PropertieApp {
+public class PropertiesApp {
 	public static void main(String[] args) {
 		
 	}
