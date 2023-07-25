dca23d4162d061fc0898f4aff2448588cae07880
tkdau <tkdau@192.168.1.8>
2023-07-25 16:06:50 +0900

    78day 07-25

--- spring/src/main/java/xyz/itwill00/log/LogHelloWorld.java
+++ spring/src/main/java/xyz/itwill00/log/LogHelloWorld.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LogHelloWorld {
-	private static final Logger logger=LoggerFactory.getLogger(LogHelloWorldApp.class);
-	
+	private static final Logger logger=LoggerFactory.getLogger(LogHelloWorld.class);
+
 	public String hello(String name) {
 		logger.info("Ω√¿€");
-		String 
+		String message=name+"¥‘, æ»≥Á«œººø‰.";
+		logger.info("¡æ∑·");
+		return message;
 	}
 }
---
+++ spring/src/main/java/xyz/itwill02/factory/HelloMessageObject.java
@@ -0,0 +1,10 @@
+package xyz.itwill02.factory;
+
+//Factory 클래스로 제공받은 객체가 변경돼도 메소드는 변경되지 않도록 반드시 인터페이스를 상속받아야 되는 인터페이스
+public class HelloMessageObject implements MessageObject {
+	@Override
+	public String getMessage() {
+		return "Hello!!!";
+	}
+
+}
