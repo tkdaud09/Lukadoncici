d30d6cc8188d8039461f9bf124306c910e48b641
tkdau <tkdau@192.168.1.8>
2023-08-01 16:41:43 +0900

    83day 08-01

--- spring/src/main/java/xyz/itwill07/aop/JoinPointBean.java
+++ spring/src/main/java/xyz/itwill07/aop/JoinPointBean.java
@@ -3,19 +3,19 @@ package xyz.itwill07.aop;
 //핵심관심모듈
 public class JoinPointBean {
 	public void add() {
-		System.out.println("*** JoinPointBean 클래스의 add() 메소드 호출 ***");		
+		System.out.println("*** JoinPointBean 클래스의 add() 메소드 호출 ***");
 	}
 	
 	public void modify(int num, String name) {
-		System.out.println("*** JoinPointBean 클래스의 modify(int num, String name) 메소드 호출 ***");		
+		System.out.println("*** JoinPointBean 클래스의 modify(int num, String name) 메소드 호출 ***");
 	}
 	
 	public void remove(int num) {
-		System.out.println("*** JoinPointBean 클래스의 remove(int num) 메소드 호출 ***");		
+		System.out.println("*** JoinPointBean 클래스의 remove(int num) 메소드 호출 ***");
 	}
 	
 	public String getName() {
-		System.out.println("*** JoinPointBean 클래스의 getName() 메소드 호출 ***");		
+		System.out.println("*** JoinPointBean 클래스의 getName() 메소드 호출 ***");
 		return "홍길동";
 	}
 	
