e75f64378c5eccf7de2d3861e23d0b916d1b699a
tkdau <tkdau@192.168.1.12>
2023-08-09 18:13:24 +0900

    89day 08-09

--- spring/src/test/java/xyz/itwill/controller/PointUserServiceTest.java
+++ spring/src/test/java/xyz/itwill/controller/PointUserServiceTest.java
@@ -1,5 +1,33 @@
 package xyz.itwill.controller;
 
-public class PointUserServiceTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
+import lombok.extern.slf4j.Slf4j;
+import xyz.itwill10.dto.PointUser;
+import xyz.itwill10.service.PointUserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+@Slf4j
+public class PointUserServiceTest {
+	@Autowired
+	private PointUserService pointUserService;
+	
+	@Transactional
+	@Test
+	public void testAddPointUser() throws Exception {
+		//PointUser user=PointUser.builder().id("abc123").name("È«±æµ¿").build();
+		PointUser user=PointUser.builder().id("xyz789").name("ÀÓ²©Á¤").build();
+		
+		PointUser addUser=pointUserService.addPointUser(user);
+
+		log.info(addUser.toString());
+	}
 }
