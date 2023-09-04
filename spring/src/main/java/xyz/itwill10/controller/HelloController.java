0f57cf21c37fe213fd734e189ebe331c2886c7c9
tkdaud09 <tkdaud509199@gmail.com>
2023-09-04 18:10:46 +0900

    09-04 105day

--- spring/src/main/java/xyz/itwill10/controller/HelloController.java
+++ spring/src/main/java/xyz/itwill10/controller/HelloController.java
@@ -7,19 +7,19 @@ import org.springframework.web.servlet.ModelAndView;
 import lombok.extern.slf4j.Slf4j;
 
 //@Controller : 요청 처리 클래스(Controller 클래스)를 Spring Bean으로 등록하기 위한 어노테이션
-// => 클래스의 이름을 Spring Bean의 식별자(beanName)로 자동 설정 - 첫문자는 소문자로 변환
+// => 클래스의 이름을 Spring Bean의 식별자(beanName)으로 자동 설정 - 첫문자는 소문자로 변환
 // => value 속성을 사용하여 Spring Bean의 식별자(beanName) 변경 가능
 
 @Slf4j
 //@Controller 어노테이션을 사용하면 Controller 인터페이스를 상속받지 않아도 요청 처리 클래스로 작성 가능
-// => @RequestMapping 어노테이션을 사용하여 메소드를 요청 처리 메소드 처리되도록 작성
+// => @RequestMapping 어노테이션을 사용하여 메소드를 요청 처리 메소드로 처리되도록 작성
 // => @RequestMapping 어노테이션을 사용하여 요청 처리 메소드를 여러개 선언 가능 
 @Controller
 public class HelloController {
 	
 	//요청 처리 메소드는 Front Controller에게 반드시 뷰이름(ViewName) 제공
 	// => Front Controller는 제공받은 뷰이름을 이용하여 ViewResolver 객체로 응답 처리되도록 변환
-	//1.메소드의 반환형을 [void]로 작성한 경우 메소드의 이름을 뷰이름으로 제공 
+	//1.메소드의 반환형을 [void]로 작성한 경우 @RequestMapping 오노테이션의 value 속성값을 뷰이름으로 제공 
 	//2.메소드의 반환형을 [String]로 작성한 경우 메소드의 반환값(문자열)을 뷰이름으로 제공 
 	//3.메소드의 반환형을 [ModelAndView]로 작성한 경우 반환된 ModelAndView 객체에 뷰이름을 저장하여 제공
 
